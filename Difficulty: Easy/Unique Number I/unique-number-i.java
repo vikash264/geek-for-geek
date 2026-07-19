494fadccbf07385c3b7f4da8eec51785ca9d8743
Vikash Kumar <vkm844508@gmail.com>
2026-07-19 09:33:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Number I/unique-number-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findUnique(int[] arr) {
+        // code here
+        int unique = 0;
+        for(int num : arr){
+            unique ^= num;
+        }
+        return unique;
+    }
+}
